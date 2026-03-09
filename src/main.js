274eaae645d2d5b58e504a792c7b3c10c1287b4c
ansh <daerrox@gmail.com>
2026-03-10 03:07:24 +0530

    updated the code flow

--- src/main.js
+++ src/main.js
@@ -2,66 +2,73 @@
 const createButtonRef = document.getElementById('createButton');
 const todoInputRef = document.getElementById('todoInput');
 const todoContainerRef = document.getElementById('todoContainer');
+let uniqueId = 1;
 let elementToBeUpdated;
 
 
-createButtonRef.addEventListener('click', function () {
-    const value = todoInputRef.value;
-    const li = document.createElement('li');
-    const buttonsDiv = document.createElement('div');
-    const TextParaTag = document.createElement('p');
-    const updateButton = document.createElement('button');
-    const deleteButton = document.createElement('button');
-    if (createButtonRef.innerText === "Create") {
 
+createButtonRef.addEventListener('click', function () {
+    if (createButtonRef.innerText === "Create") {
+        const value = todoInputRef.value;
         if (value === "") {
-            alert("enter something please")
+            alert("please enter something")
             return;
         }
-
-        let completed = true;
-
-        updateButton.id = "updateButton"
-        deleteButton.id = "deleteButton"
-        updateButton.innerText = "Update"
-        deleteButton.innerText = "Delete"
-        updateButton.addEventListener("click", function () {
-            todoInputRef.value = TextParaTag.innerText;
-            createButtonRef.innerText = "Save";
-            elementToBeUpdated = TextParaTag;
-            createButtonRef.style.backgroundColor = "#7dbc77"
-
-        })
-        deleteButton.addEventListener("click", function () {
-            li.remove();
-        })
-        buttonsDiv.appendChild(updateButton)
-        buttonsDiv.appendChild(deleteButton)
-
-        TextParaTag.innerText = value;
-        li.appendChild(TextParaTag)
-        li.appendChild(buttonsDiv)
-        TextParaTag.addEventListener('click', function () {
-            TextParaTag.style.fontStyle = completed ? "italic" : "normal";
-            TextParaTag.style.textDecoration = completed ? "line-through" : "none";
-            li.style.backgroundColor = completed ? "#8FE388" : "white";
-            completed = !completed;
-        });
+        const li = document.createElement('li');
+        li.id = `todoId${uniqueId}`
+        li.innerHTML = `
+        <div class="valueContainer">
+            <div class="false"></div>
+            <p>${value}</p>
+        </div>
+        <div class="buttonContainer">
+            <button class="updateButton">Update</button>
+            <button class="deleteButton" >Delete</button>
+        </div>`
 
         todoContainerRef.appendChild(li);
-        todoInputRef.value = '';
+        uniqueId++;
+        todoInputRef.value = ""
     }
     else if (createButtonRef.innerText === "Save") {
-        elementToBeUpdated.innerText = value;
-        createButtonRef.innerText = "Create";
+        const value = todoInputRef.value;
+        elementToBeUpdated.querySelector("p").innerText = value;
+        createButtonRef.innerText = "Create"
+        todoInputRef.value = ""
         createButtonRef.style.backgroundColor = "#4361ee"
-        todoInputRef.value = '';
     }
-    else {
-        console.log("error error everywhere");
-    }
+    elementToBeUpdated = "";
+})
 
 
-});
+
+todoContainerRef.addEventListener('click', (e) => {
+    if (e.target.className === "deleteButton") {
+        e.target.parentNode.parentNode.remove()
+    }
+    else if (e.target.className === "updateButton") {
+        createButtonRef.innerText = "Save";
+        createButtonRef.style.backgroundColor = "#52d152bf"
+        elementToBeUpdated = e.target.parentNode.parentNode;
+        todoInputRef.value = elementToBeUpdated.querySelector("p").innerText;
+    }
+})
+
+todoContainerRef.addEventListener('click', (e) => {
+    if (e.target.className === "true") {
+        e.target.className = "false";
+        e.target.parentNode.parentNode.querySelector("p").style.fontStyle = "normal"
+        e.target.parentNode.parentNode.querySelector("p").style.textDecoration = "none"
+        e.target.parentNode.parentNode.style.backgroundColor = "white"
+    }
+    else if (e.target.className === "false") {
+        e.target.className = "true"
+        e.target.parentNode.parentNode.querySelector("p").style.fontStyle = "italic"
+        e.target.parentNode.parentNode.querySelector("p").style.textDecoration = "line-through"
+        e.target.parentNode.parentNode.style.backgroundColor = "#52d152bf"
+    }
+})
+
+
 
 
